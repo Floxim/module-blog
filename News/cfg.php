8037ee312d9201c0ce9efcfc1f82d9d65d75e695
Ilya <dubr.cola@gmail.com>
2016-06-28 19:34:51 +0300

    news config

---
+++ News/cfg.php
@@ -0,0 +1,14 @@
+<?php
+
+use Floxim\Floxim\System\Fx as fx;
+
+return array(
+    'actions' => array(
+        '*list*' => array(
+            'defaults' => array(
+                'sorting' => 'publish_date',
+                'sorting_dir' => 'desc'
+            ),
+        )
+    )
+);
